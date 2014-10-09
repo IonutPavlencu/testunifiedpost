fc4aa309ef1e58de02a4158ba196496f4a9ca3cf
Ionut Pavlencu <ionut.pavlencu@gmail.com>
2014-10-09 08:30:22 -0400

    V 1.2.1

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,7 +2,7 @@ class FormMailer < ActionMailer::Base
     default from: "test@goodnet.ro"
   def admin(form)
     @form = form
-    mail to: "ionut.pavlencu@gmail.com" , subject: "New subscriber"
+    mail to: "ciprian.csordas@unifiedpost.com" , subject: "New subscriber"
   end
   def subscriber(form)
     @form = form
